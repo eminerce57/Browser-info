bb3603b8cfb2bbcb206fdf72dbf5b1d21be17ea4
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-07-29 14:42:38 +0300

    update I added platform info

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,39 @@
 console.log(navigator)
 console.log(window)
+
+
+info()
+
+
+function info(){
+document.getElementById("OS").innerHTML += `
+${Osimage(navigator.platform)}
+
+
+`
+
+
+}
+
+function Osimage(platform){
+
+    console.log(platform)
+
+    if (navigator.platform.indexOf("win")) {
+        console.log("win")
+        return `<img class="img-responsive"  src="img/win-logo.png">`
+    }
+    else if (navigator.platform.indexOf("mac")) {
+        console.log("mac")
+        return `<img class="img-responsive"  src="img/apple-logo.png">`
+    }
+    else if (navigator.platform.indexOf("linux")) {
+
+        console.log("linux")
+        return `<img class="img-responsive"  src="img/linux-logo.png">`
+    }
+    else{
+        return "unknown"
+    }
+
+}
